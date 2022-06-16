86bc86060836d2175a96956f1b725ad6721ba839
0xFable <0xfable@protonmail.com>
2022-06-16 23:18:43 +0100

    feat(DexDriver): Add methods to DexDriver trait

--- packages/cw-test-harness/src/traits/traits.rs
+++ packages/cw-test-harness/src/traits/traits.rs
@@ -1,5 +1,7 @@
-use cosmwasm_std::{Addr};
+use cosmwasm_std::{Addr, Uint128};
 use cw_multi_test::{App};
+use anyhow::Result as AnyResult;
+
 // A Driver is a trait which other implementors must implement to achieve a common schema across Drivers
 pub trait Driver {
     fn init_contracts(router: &mut App, owner: &Addr) -> Self;
@@ -7,8 +9,9 @@ pub trait Driver {
 
 // A DexDriver is a trait which includes common operations which usually have similar inputs 
 pub trait DexDriver {
-    fn deposit_liquidity();
-    fn withdraw_liquidity();
-    fn create_pair();
+    fn deposit_liquidity(&self, app: &mut App, pair_instance: &Addr, token_x_instance: &Addr, token_x_amount: Uint128, token_y_instance: &Addr, token_y_amount: Uint128, caller: &Option<Addr>);
+    fn withdraw_liquidity(&self, app: &mut App, pair_instance: &Addr, lp_token_instance: &Addr, lp_token_amount: Uint128, caller: &Option<Addr>);
+    fn create_pool(&self, app: &mut App, token1: &Addr, token2: &Addr) -> AnyResult<Addr>;
+    fn init_cw20_token(&self, app: &mut App, name: &str) -> AnyResult<Addr>;
 }
 
